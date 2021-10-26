c4b101c21f103847d534644f14c1eaf56acde339
Guruprerana Shabadi <guruprerana@gmail.com>
2021-10-26 17:20:32 +0200

    fixed unit prop on dpll

--- Solver/DPLL.hs
+++ Solver/DPLL.hs
@@ -9,6 +9,10 @@ import Solver.Utils
 import Data.List
 import Data.Maybe
 
+{-
+
+------- DEPRECATED IMPLEMENTATION OF UNIT PROP ----------
+
 -- performs unit propagation on the cnf
 unitProp :: CNF -> (CNF, [Subst] -> [Subst])
 unitProp cnf =
@@ -24,23 +28,48 @@ unitProp cnf =
     firstLit :: Cls -> Lit
     firstLit = head . literals
 
-solutions :: CNF -> [Subst]
-solutions cnf =
+solutions' :: CNF -> [Subst]
+solutions' cnf =
   case clauses $ fst $ unitProp cnf of
     [] -> [[]] -- trivially satisfiable
     clss ->
       case (BigOr []) `elem` clss of
         True -> [] -- empty conjunction unsatisfiable
         False ->
-          (unitP $ addLit l $ solutions $ conditionCNF l cnf') ++
+          (unitP $ addLit l $ solutions' $ conditionCNF l cnf') ++
           (if needed
-             then (unitP $ addLit opl $ solutions $ conditionCNF opl cnf')
+             then (unitP $ addLit opl $ solutions' $ conditionCNF opl cnf')
              else [])
           where l = head $ literals $ head clss
                 opl = Lit (var l) (not $ pol l)
                 (cnf', unitP) = unitProp cnf
                 needed :: Bool
-                needed = isLitIn cnf' opl
+                needed = True
+                --needed = isLitIn cnf' opl
+
+-------------------------------------------------------------
+-}
+
+unitCls :: CNF -> [Lit]
+unitCls cnf = map firstLit $ filter (\(BigOr lits) -> length lits == 1) $ clauses cnf
+              where
+                firstLit = head . literals
+
+solutions :: CNF -> [Subst]
+solutions cnf =
+  case clauses cnf of
+    [] -> [[]] -- trivially satisfiable
+    clss ->
+      case (BigOr []) `elem` clss of
+        True -> [] -- empty conjunction unsatisfiable
+        False ->
+          case unitCls cnf of
+            l:_ -> addLit l $ solutions $ conditionCNF l cnf
+            [] -> (addLit l $ solutions $ conditionCNF l cnf) ++ (if needed then (addLit l $ solutions $ conditionCNF l cnf) else [])
+            where
+              l = head $ literals $ head clss
+              opl = Lit (var l) (not $ pol l)
+              needed = isLitIn cnf opl
 
 -- we also add an extra filter to cross check we only have correct solutions (for testing)
 solution :: CNF -> Maybe Subst
